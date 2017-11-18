e73d126460a1b952953e0fe8e36076be4f64fadf
PotatoGG <512006634@qq.com>
2017-11-18 11:08:58 +0800

    table  全选

--- src/app/pages/ng-bootstrap/ng-bootstrap.component.ts
+++ src/app/pages/ng-bootstrap/ng-bootstrap.component.ts
@@ -13,14 +13,25 @@ export class NgBootstrapComponent implements OnInit {
     {id:1,eName:'1',cName:'2'},
     {id:2,eName:'1',cName:'2'},
     {id:3,eName:'1',cName:'2'},
+    {id:4,eName:'1',cName:'2'},
+    {id:5,eName:'1',cName:'2'},
+    {id:6,eName:'1',cName:'2'},
+    {id:7,eName:'1',cName:'2'},
   ];
   selectedtableDate=[];
-  selectecurrentPages=[];
   currentPage:number=1;
   pageSize:number=2;
+  currentPages=[];
+  pageData=[];
+  pageCount:number;
   constructor(private modalservice:NgbModal) { }
 
   ngOnInit() {
+    this.pageCount=this.tableDate.length/this.pageSize;
+
+    if(this.tableDate.length%this.pageSize!=0){
+      this.pageCount=Number((this.tableDate.length/this.pageSize).toFixed(0));
+    }
   }
   openModal(){
     const ngbModalRef:NgbModalRef=this.modalservice.open(ModalComponent,
@@ -48,29 +59,84 @@ export class NgBootstrapComponent implements OnInit {
     }
     console.log(this.selectedtableDate);
   }
-  selectThisPage(ifChecked:boolean,currentPage:any){
-    let index=this.selectecurrentPages.indexOf(currentPage);
+  getThisPageData(currentPage:any){
+    var thisPageData=[];
+    if(currentPage==this.pageCount){//假设不能整除，且是最后一页
+      var num=this.pageSize-(currentPage*this.pageSize-this.tableDate.length);//最后一页实际个数
+      var end=this.tableDate.length;
+      var start=end-num;
+      for(start;start<=end-1;start++){
+        // this.selectedtableDate.push(this.tableDate[start].id);
+        thisPageData.push(this.tableDate[start].id);
+      }
+      this.pageData.push({currentPage:currentPage,data:thisPageData});
+      // console.log('pageData push1');
+    }
+    else{
+      let endIndex=currentPage*this.pageSize;//把本页放进去
+      let s=endIndex-this.pageSize;
+      for(s;s<=endIndex-1;s++){
+        // this.selectedtableDate.push(this.tableDate[s].id);
+        thisPageData.push(this.tableDate[s].id);
+      }
+      this.pageData.push({currentPage:currentPage,data:thisPageData});
+      // console.log('pageData push2');
+    }
+  }
+  selectThisPage(ifChecked:boolean,currentPage:number){
+    let index=this.currentPages.indexOf(currentPage);
+    this.getThisPageData(currentPage);
     if(ifChecked){
       if(index==-1){
-        this.selectecurrentPages.push(currentPage);
+       this.currentPages.push(currentPage);
+       console.log(this.pageData);
+       for(let item of  this.pageData){
+         if(item.currentPage==currentPage){
+           for(let i of item.data){
+             this.selectedtableDate.push(i);
+           }
+         }
+       }
+       console.log(this.selectedtableDate.length);
+       console.log(this.tableDate.length);
       }
     }
     else{
-      this.selectecurrentPages.splice(index,1);
+      // let thisPageData=[];
+      this.currentPages.splice(index,1);
+      for(let item of  this.pageData){
+        if(item.currentPage==currentPage){
+          for(var i=0;i<this.selectedtableDate.length;i++){
+            for(let j of item.data){
+              if(j==this.selectedtableDate[i]){
+                console.log(j);
+                this.selectedtableDate.splice(i,1);
+              }
+            }
+          }
+        }
+      }
     }
-    console.log(this.selectecurrentPages);
+    // console.log(this.currentPages);
   }
+
   selectAll(ifChecked:boolean){
     if(ifChecked){
       this.selectedtableDate=[];
       this.tableDate.forEach((value)=>{
         this.selectedtableDate.push(value.id);
-      })
+      });
+
+      for(var i=1;i<=this.pageCount;i++){
+        this.currentPages.push(i);
+      }
+
     }
     else{
       this.selectedtableDate=[];
+      this.currentPages=[]
     }
-    console.log(this.selectedtableDate);
+    // console.log(this.selectedtableDate);
   }
   pageChanged(page:any){
     this.currentPage=page;
