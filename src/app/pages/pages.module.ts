47e9821be6bd3ca5e77922c0930e381f5380b381
PotatoGG <512006634@qq.com>
2017-11-24 19:01:24 +0800

    .

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,8 @@ import { ModalComponent } from './ng-bootstrap/modal/modal.component';
 import {NgxPaginationModule} from "ngx-pagination";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { SweetAlertComponent } from './sweet-alert/sweet-alert.component';
+import { DragulaComponent } from './dragula/dragula.component';
+import {DragulaModule} from "ng2-dragula";
 @NgModule({
   imports: [
     CommonModule,
@@ -19,10 +21,11 @@ import { SweetAlertComponent } from './sweet-alert/sweet-alert.component';
     ReactiveFormsModule,
     NgbModule.forRoot(),
     NgxPaginationModule,
+    DragulaModule,
     routing
   ],
   providers:[],
   entryComponents:[ModalComponent],
-  declarations: [PagesComponent, SidebarComponent, LoadingComponent, NgBootstrapComponent, ModalComponent, SweetAlertComponent]
+  declarations: [PagesComponent, SidebarComponent, LoadingComponent, NgBootstrapComponent, ModalComponent, SweetAlertComponent, DragulaComponent]
 })
 export class PagesModule { }
