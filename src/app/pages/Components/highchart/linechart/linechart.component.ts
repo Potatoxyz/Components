eb240571b31b9eae8de67059772c35d869aa6133
PotatoGG <512006634@qq.com>
2017-12-22 18:32:50 +0800

    hightchart

---
+++ src/app/pages/Components/highchart/linechart/linechart.component.ts
@@ -0,0 +1,63 @@
+import {Component, Input, OnInit} from '@angular/core';
+import * as theme from '../chart.theme';
+import * as Highcharts from 'highcharts';
+@Component({
+  selector: 'app-linechart',
+  templateUrl: './linechart.component.html',
+  styleUrls: ['./linechart.component.scss']
+})
+export class LinechartComponent implements OnInit {
+  @Input() title:string='';
+  constructor() { }
+
+  ngOnInit() {
+    this.initLineChart();
+  }
+
+  initLineChart(){
+    let options={
+      //图表所有类型在plotOptions下显示有
+      title: {
+        text: '2010 ~ 2016 年太阳能行业就业人员发展情况'
+      },
+      subtitle: {
+        text: '数据来源：thesolarfoundation.com'
+      },
+      chart: {type: 'line'},//默认也是line
+      //图利
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+      },
+        yAxis: {
+          title: {
+            text: '就业人数'
+          }
+        },
+      //X轴数据列如果没有指定的通用默认设置
+      plotOptions: {
+        //X轴去点数据，适合增长的单一数字
+        series: {pointStart: 2010}
+      },
+      series: [{
+        name: '安装，实施人员',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+      }, {
+        name: '工人',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+      }, {
+        name: '销售',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+      }, {
+        name: '项目开发',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+      }, {
+        name: '其他',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+      }],
+    };
+    Highcharts.setOptions(theme.getPublicChartTheme());
+    Highcharts.chart('container',options);
+  }
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,31 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {PagesComponent} from "./pages.component";
 import {routing} from "./pages.routing";
-import { SidebarComponent } from './sidebar/sidebar.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
 import {RouterModule} from "@angular/router";
-import { LoadingComponent } from './loading/loading.component';
-import { NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import { NgBootstrapComponent } from './ng-bootstrap/ng-bootstrap.component';
-import { ModalComponent } from './ng-bootstrap/modal/modal.component';
+import {LoadingComponent} from './loading/loading.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgBootstrapComponent} from './ng-bootstrap/ng-bootstrap.component';
+import {ModalComponent} from './ng-bootstrap/modal/modal.component';
 import {NgxPaginationModule} from "ngx-pagination";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { SweetAlertComponent } from './sweet-alert/sweet-alert.component';
-import { AnimationComponent } from './animation/animation.component';
+import {SweetAlertComponent} from './sweet-alert/sweet-alert.component';
+import {AnimationComponent} from './animation/animation.component';
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { CommonComponent } from './common/common.component';
+import {CommonComponent} from './common/common.component';
 import {HttpClientModule} from "@angular/common/http";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "../../shared/http/in-memory-data.service";
-import { HttpComponent } from './http/http.component';
+import {HttpComponent} from './http/http.component';
 import {Public_api} from "../../shared/http/public_api";
 import {MydirectiveModule} from "../../shared/directve/mydirective.module";
 import {Select2Module} from "ng2-select2";
 import {AnywereService} from "./anywere.service";
 import {SortPipe} from "../../shared/pipe/sort.pipe";
-import { CommonHeadComponent } from './Components/common-head/common-head.component';
-import { NgDatepickerModule } from 'ng2-datepicker'
+import {CommonHeadComponent} from './Components/common-head/common-head.component';
+import {NgDatepickerModule} from 'ng2-datepicker';
+import {LinechartComponent} from "./Components/highchart/linechart/linechart.component";
+
 
 @NgModule({
   imports: [
@@ -41,14 +43,15 @@ import { NgDatepickerModule } from 'ng2-datepicker'
     HttpClientModule,
     NgDatepickerModule,
     HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
+      InMemoryDataService, {dataEncapsulation: false}
     ),
     MydirectiveModule,
     routing
   ],
-  providers:[Public_api,AnywereService],
-  entryComponents:[ModalComponent],
-  declarations: [SortPipe,PagesComponent, SidebarComponent, LoadingComponent,
-    NgBootstrapComponent, ModalComponent, SweetAlertComponent, AnimationComponent, CommonComponent, HttpComponent, CommonHeadComponent]
+  providers: [Public_api, AnywereService],
+  entryComponents: [ModalComponent],
+  declarations: [SortPipe, PagesComponent, SidebarComponent, LoadingComponent,
+    NgBootstrapComponent, ModalComponent, SweetAlertComponent, AnimationComponent, CommonComponent, HttpComponent, CommonHeadComponent, LinechartComponent]
 })
-export class PagesModule { }
+export class PagesModule {
+}
