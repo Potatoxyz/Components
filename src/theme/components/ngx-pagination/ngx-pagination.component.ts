65c4715a376c3fb290bc48f38a0114a445625746
陈晖 <512006634@qq.com>
2018-07-31 16:12:44 +0800

    themeModule

---
+++ src/theme/components/ngx-pagination/ngx-pagination.component.ts
@@ -0,0 +1,20 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {PageInfoModel} from "../../../../shared/Models/page-list.model";
+
+@Component({
+  selector: 'app-ngx-pagination',
+  templateUrl: './ngx-pagination.html',
+  styleUrls: ['./ngx-pagination.scss']
+})
+export class NgxPaginationComponent {
+  @Input() PageInfo:PageInfoModel;
+  @Output() Load:EventEmitter<any>=new EventEmitter<any>();
+  constructor() {}
+  pageChanged(pn:number){
+    this.PageInfo.pageIndex=pn;
+    this.Load.emit(pn);
+  }
+  loadData(pn:number=1){
+    this.Load.emit(1);
+  }
+}
